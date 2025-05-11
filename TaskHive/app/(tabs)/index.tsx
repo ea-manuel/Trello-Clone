bfe862a7c9e0f6db1599386b2c57b2deb47a832f
Emma. A <immajacobs2003@gmail.com>
2025-05-12 00:30:36 +0100

    Update index.tsx

--- TaskHive/app/(tabs)/index.tsx
+++ TaskHive/app/(tabs)/index.tsx
@@ -1,10 +1,5 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
-import { registerRootComponent } from 'expo';
 
-// Register the HomeScreen as the root component of the app
-registerRootComponent(HomeScreen);
-
+export default function HomeTab() {
+  return <HomeScreen />;
+}
